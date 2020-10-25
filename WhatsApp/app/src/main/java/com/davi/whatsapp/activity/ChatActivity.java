af64f47f10c31ac739f94d697ee28887f03d3ebf
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2020-10-25 14:20:35 -0300

    Adicionado a Classe de Mensagens e configurado o envio de mensagem na classe ChatActivity.

--- WhatsApp/app/src/main/java/com/davi/whatsapp/activity/ChatActivity.java
+++ WhatsApp/app/src/main/java/com/davi/whatsapp/activity/ChatActivity.java
@@ -4,6 +4,10 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import com.bumptech.glide.Glide;
+import com.davi.whatsapp.config.ConfiguracaoFirebase;
+import com.davi.whatsapp.helper.Base64Custom;
+import com.davi.whatsapp.helper.UsuarioFirebase;
+import com.davi.whatsapp.model.Mensagem;
 import com.davi.whatsapp.model.Usuario;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -12,9 +16,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.davi.whatsapp.R;
+import com.google.firebase.database.DatabaseReference;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -22,6 +29,11 @@ public class ChatActivity extends AppCompatActivity {
     private TextView textViewNome;
     private CircleImageView circleImageViewFoto;
     private Usuario usuarioDestinatario;
+    private EditText editMensagem;
+
+    //identificador usuarios remetente e destinatário
+    private String idUsuarioRemetente;
+    private String idUsuarioDestinatario;
 
 
     @Override
@@ -39,6 +51,10 @@ public class ChatActivity extends AppCompatActivity {
         //Configurações iniciais
         textViewNome = findViewById(R.id.textViewNomeChat);
         circleImageViewFoto =findViewById(R.id.circleImageFotoChat);
+        editMensagem = findViewById(R.id.editMensagem);
+
+        //Recupera dados do usuário remetente
+        idUsuarioRemetente = UsuarioFirebase.getIdentificadorUsuario();
 
         //Recuperar dados do usuário destinatário
         Bundle bundle = getIntent().getExtras();
@@ -54,8 +70,38 @@ public class ChatActivity extends AppCompatActivity {
             }else{
                 circleImageViewFoto.setImageResource(R.drawable.padrao);
             }
+            //Recuperar dados usuario destinatario
+            idUsuarioDestinatario = Base64Custom.codificarBase64(usuarioDestinatario.getEmail());
         }
 
+
+    }
+
+    public void enviarMensagem(View view){
+        String textoMensagem = editMensagem.getText().toString();
+        if(!textoMensagem.isEmpty()){
+            Mensagem mensagem = new Mensagem();
+            mensagem.setIdUsuario(idUsuarioRemetente);
+            mensagem.setMensagem(textoMensagem);
+
+            //Salvar Mensagem
+            salvarMensagem(idUsuarioRemetente,idUsuarioDestinatario,mensagem);
+
+        }else{
+            Toast.makeText(ChatActivity.this, "Digite uma mensagem para enviar!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void salvarMensagem(String idRemetente,String idDestinatario, Mensagem msg){
+        DatabaseReference database = ConfiguracaoFirebase.getFirebaseDatabase();
+        DatabaseReference mensagemRef = database.child("mensagens");
+        mensagemRef.child(idRemetente)
+                .child(idDestinatario)
+                .push()
+                .setValue(msg);
+
+        //Limpar texto
+        editMensagem.setText("");
 
     }
 }
---
+++ WhatsApp/app/src/main/java/com/davi/whatsapp/model/Mensagem.java
@@ -0,0 +1,32 @@
+package com.davi.whatsapp.model;
+
+public class Mensagem {
+    private String mensagem,idUsuario,imagem;
+
+    public Mensagem() {
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+}

5598f8f98221a4c55901ee1f326fb82c661b3e63
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2020-11-05 16:20:03 -0300

    Listando a conversa e abrindo a mesma quando for clicada.

--- WhatsApp/app/src/main/java/com/davi/whatsapp/fragment/ConversasFragment.java
+++ WhatsApp/app/src/main/java/com/davi/whatsapp/fragment/ConversasFragment.java
@@ -1,21 +1,46 @@
 package com.davi.whatsapp.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import com.davi.whatsapp.R;
+import com.davi.whatsapp.activity.ChatActivity;
+import com.davi.whatsapp.adapter.ConversasAdapter;
+import com.davi.whatsapp.config.ConfiguracaoFirebase;
+import com.davi.whatsapp.helper.RecyclerItemClickListener;
+import com.davi.whatsapp.helper.UsuarioFirebase;
+import com.davi.whatsapp.model.Conversa;
+import com.davi.whatsapp.model.Usuario;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ConversasFragment extends Fragment {
-
+ private RecyclerView recyclerViewConversas;
+ private List<Conversa>listaConversas = new ArrayList<>();
+ private ConversasAdapter adapter;
+ private DatabaseReference database;
+ private  DatabaseReference conversasRef;
+ private ChildEventListener childEventListenerConversas;
 
     public ConversasFragment() {
         // Required empty public constructor
@@ -26,7 +51,100 @@ public class ConversasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_conversas, container, false);
+        View view = inflater.inflate(R.layout.fragment_conversas, container, false);
+        recyclerViewConversas = view.findViewById(R.id.recyclerListaConversas);
+
+        //Configurar adapter
+
+        adapter = new ConversasAdapter(listaConversas,getActivity());
+        //Configurar recyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerViewConversas.setLayoutManager(layoutManager);
+        recyclerViewConversas.setHasFixedSize(true);
+        recyclerViewConversas.setAdapter(adapter);
+
+        //Configurar evento de clique
+        recyclerViewConversas.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getActivity(),
+                        recyclerViewConversas,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Conversa conversaSelecionada = listaConversas.get(position);
+                                Intent i = new Intent(getActivity(), ChatActivity.class);
+                                i.putExtra("chatContato",conversaSelecionada.getUsuarioExibicao());
+                                startActivity(i);
+
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            }
+                        }
+                ));
+
+        //Configura conversas ref
+        String identificadorUsuario = UsuarioFirebase.getIdentificadorUsuario();
+        database = ConfiguracaoFirebase.getFirebaseDatabase();
+        conversasRef = database.child("conversas")
+                .child(identificadorUsuario);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        recuperarConversas();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        conversasRef.removeEventListener(childEventListenerConversas);
+    }
+
+    public void recuperarConversas(){
+
+
+        childEventListenerConversas = conversasRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                //Recuperar Conversas
+                Conversa conversa = dataSnapshot.getValue(Conversa.class);
+                listaConversas.add(conversa);
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }

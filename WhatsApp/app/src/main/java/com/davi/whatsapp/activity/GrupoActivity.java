f74bb0507286921b7d7d700b736e53732be8b974
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2020-11-06 17:07:42 -0300

    Listando contatos disponível para o grupo.

--- WhatsApp/app/src/main/java/com/davi/whatsapp/activity/GrupoActivity.java
+++ WhatsApp/app/src/main/java/com/davi/whatsapp/activity/GrupoActivity.java
@@ -2,17 +2,38 @@ package com.davi.whatsapp.activity;
 
 import android.os.Bundle;
 
+import com.davi.whatsapp.adapter.ContatosAdapter;
+import com.davi.whatsapp.config.ConfiguracaoFirebase;
+import com.davi.whatsapp.helper.UsuarioFirebase;
+import com.davi.whatsapp.model.Usuario;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
 import com.davi.whatsapp.R;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GrupoActivity extends AppCompatActivity {
+    private RecyclerView recyclerMembrosSelecionados, recyclerMembros;
+    private ContatosAdapter contatosAdapter;
+    private List<Usuario> listaMembros = new ArrayList<>();
+    private ValueEventListener valueEventListenerMembros;
+    private DatabaseReference usuarioRef;
+    private FirebaseUser usuarioAtual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +50,56 @@ public class GrupoActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Confugurações Iniciais
+        recyclerMembrosSelecionados = findViewById(R.id.recyclerMembrosSelecionados);
+        recyclerMembros = findViewById(R.id.recyclerMembros);
+        usuarioRef = ConfiguracaoFirebase.getFirebaseDatabase().child("usuarios");
+        usuarioAtual = UsuarioFirebase.getUsuarioAtual();
+
+        //Configurando adapter
+        contatosAdapter = new ContatosAdapter(listaMembros,getApplicationContext());
+
+        //Configurando RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerMembros.setLayoutManager(layoutManager);
+        recyclerMembros.setHasFixedSize(true);
+        recyclerMembros.setAdapter(contatosAdapter);
+    }
+
+    public void recuperarContatos(){
+        valueEventListenerMembros = usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot dados: dataSnapshot.getChildren()){
+
+                    Usuario usuario = dados.getValue(Usuario.class);
+                    String emailUsuarioAtual = usuarioAtual.getEmail();
+                    if(!emailUsuarioAtual.equals(usuario.getEmail())){
+                        listaMembros.add(usuario);
+                    }
+
+                }
+                contatosAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        recuperarContatos();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        usuarioRef.removeEventListener(valueEventListenerMembros);
     }
 }
